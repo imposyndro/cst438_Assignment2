022e5e7eefa000cee658952a518d91282b93b1d3
Aaron Bourdeaux <abourdeaux@csumb.edu>
2024-07-02 13:53:21 -0400

    Revert "Completed entity classes for Enrollment, Assignment, and Grade"
    This reverts commit 52ca6bf1f10042954ba10f261e6481e0d011f5cc.

--- src/main/java/com/cst438/domain/Enrollment.java
+++ src/main/java/com/cst438/domain/Enrollment.java
@@ -8,10 +8,28 @@ public class Enrollment {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="enrollment_id")
     int enrollmentId;
-	
-	// TODO complete this class
-    // add additional attribute for grade
-    // create relationship between enrollment and user entities
-    // create relationship between enrollment and section entities
-    // add getter/setter methods
+
+    @ManyToOne
+    @JoinColumn(name="section_no", nullable=false)
+    private Section section;
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable=false)
+    private User user;
+    private String grade;
+
+    public int getEnrollmentId() { return enrollmentId; }
+
+    public void setEnrollmentId(int enrollmentId) { this.enrollmentId = enrollmentId; }
+
+    public Section getSection() { return section; }
+
+    public void setSection(Section section) { this.section = section; }
+
+    public User getUser() { return user; }
+
+    public void setUser(User user) { this.user = user; }
+
+    public String getGrade() { return grade; }
+
+    public void setGrade(String grade) { this.grade = grade; }
 }
