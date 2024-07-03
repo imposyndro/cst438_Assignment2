3c4fcd23d3955def87f8c9d81af6518df5ea9013
Rahim Sarpas <127182093+sarp8099@users.noreply.github.com>
2024-07-02 19:30:26 -0700

    Update Assignment.java

--- src/main/java/com/cst438/domain/Assignment.java
+++ src/main/java/com/cst438/domain/Assignment.java
@@ -11,40 +11,9 @@ public class Assignment {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="assignment_id")
     private int assignmentId;
-
-    @ManyToOne
-    @JoinColumn(name="section_no", nullable=false)
-    private Section section;
-    private String title;
-    @Column(name="due_date")
-    private Date dueDate;
-
-    public int getAssignmentId() { return assignmentId; }
-
-    public void setAssignmentId(int assignmentId) { this.assignmentId = assignmentId; }
-
-    public Section getSection() {
-        return section;
-    }
-
-    public void setSection(Section section) {
-        this.section = section;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Date getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(Date dueDate) {
-        this.dueDate = dueDate;
-    }
-
+ 
+    // TODO  complete this class
+    // add additional attributes for title, dueDate
+    // add relationship between assignment and section entities
+    // add getter and setter methods
 }
