74545304ee21461e352f6f59abf07c5bc9e62ab2
Rahim Sarpas <127182093+sarp8099@users.noreply.github.com>
2024-07-02 19:31:12 -0700

    Update EnrollmentController.java

--- src/main/java/com/cst438/controller/EnrollmentController.java
+++ src/main/java/com/cst438/controller/EnrollmentController.java
@@ -1,67 +1,42 @@
 package com.cst438.controller;
 
 
-import com.cst438.domain.Section;
-import com.cst438.domain.SectionRepository;
 import com.cst438.dto.EnrollmentDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import com.cst438.domain.EnrollmentRepository;
-import com.cst438.domain.Enrollment;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class EnrollmentController {
-    @Autowired
-    private EnrollmentRepository enrollmentRepository;
-
-    @Autowired
-    private SectionRepository sectionRepository;
 
     // instructor downloads student enrollments for a section, ordered by student name
     // user must be instructor for the section
     @GetMapping("/sections/{sectionNo}/enrollments")
     public List<EnrollmentDTO> getEnrollments(
             @PathVariable("sectionNo") int sectionNo ) {
-        Section section = sectionRepository.findById(sectionNo).orElseThrow(()
-                -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Section not found"));;
-        List<Enrollment> enrollments = enrollmentRepository.findEnrollmentsBySectionNoOrderByStudentName(sectionNo);
-        List<EnrollmentDTO> enrollmentDTOs = new ArrayList<>();
-        for (Enrollment enrollment : enrollments) {
-            enrollmentDTOs.add(new EnrollmentDTO(
-                    enrollment.getEnrollmentId(),
-                    enrollment.getGrade(),
-                    enrollment.getUser().getId(),
-                    enrollment.getUser().getName(),
-                    enrollment.getUser().getEmail(),
-                    enrollment.getSection().getCourse().getCourseId(),
-                    enrollment.getSection().getCourse().getTitle(),
-                    enrollment.getSection().getSecId(),
-                    enrollment.getSection().getSectionNo(),
-                    enrollment.getSection().getBuilding(),
-                    enrollment.getSection().getRoom(),
-                    enrollment.getSection().getTimes(),
-                    enrollment.getSection().getCourse().getCredits(),
-                    enrollment.getSection().getTerm().getYear(),
-                    enrollment.getSection().getTerm().getSemester()
-            ));
-        }
-        return enrollmentDTOs;
+
+        // TODO
+		//  hint: use enrollment repository findEnrollmentsBySectionNoOrderByStudentName method
+        //  remove the following line when done
+        return null;
     }
 
     // instructor uploads enrollments with the final grades for the section
     // user must be instructor for the section
     @PutMapping("/enrollments")
     public void updateEnrollmentGrade(@RequestBody List<EnrollmentDTO> dlist) {
-        for (EnrollmentDTO dto : dlist) {
-            Enrollment enrollment = enrollmentRepository.findById(dto.enrollmentId()).orElseThrow(()
-                    -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Enrollment not found"));
-            enrollment.setGrade(dto.grade());
-            enrollmentRepository.save(enrollment);
-        }
+
+        // TODO
+
+        // For each EnrollmentDTO in the list
+        //  find the Enrollment entity using enrollmentId
+        //  update the grade and save back to database
+
     }
+
 }
