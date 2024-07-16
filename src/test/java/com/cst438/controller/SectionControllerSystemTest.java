7ab73ec62220cc1264b2320ea54e7ba1f79280bc
Rahim Sarpas <127182093+sarp8099@users.noreply.github.com>
2024-07-16 15:12:56 -0700

    Update SectionControllerSystemTest.java
    Added Selenium Test #1

--- src/test/java/com/cst438/controller/SectionControllerSystemTest.java
+++ src/test/java/com/cst438/controller/SectionControllerSystemTest.java
@@ -264,4 +264,160 @@ public class SectionControllerSystemTest {
        assertThrows(NoSuchElementException.class, () ->
                driver.findElement(By.xpath("//tr[td='cst499']")));
     }
+
+    // --- INSTRUCTOR TESTS START ---
+    // instructor adds a new assignment successfully
+    @Test
+    public void systemTestAddAssignment() throws Exception {
+        // Navigate to Sections
+        WebElement we = driver.findElement(By.id("sections"));
+        we.click();
+        Thread.sleep(SLEEP_DURATION);
+
+        // Enter Year and Semester
+        driver.findElement(By.id("year")).sendKeys("2024");
+        driver.findElement(By.id("semester")).sendKeys("Spring");
+        Thread.sleep(SLEEP_DURATION);
+
+        // Search table for specific course and course section
+        // Update index value if found else catch exception
+        int idx = -1;
+        // Grab Table and populate a list with it's rows
+        WebElement table = driver.findElement(By.id("sectionTable"));
+        List<WebElement> rows = table.findElements(By.tagName("tr"));
+        // Go through table for specific course section
+        try {
+            // Declare boolean for found row
+            boolean found = false;
+
+            for (int i = 0; i < rows.size() && !found; i++) {
+                WebElement row = rows.get(i);
+
+                // Grab every cell via column in the current row
+                List<WebElement> cells = row.findElements(By.tagName("td"));
+
+                // Failsafe check in case row has less than 3 cells
+                if (cells.size() >= 3) {
+                    WebElement secondCell = cells.get(1);
+                    WebElement thirdCell = cells.get(2);
+                    // Convert to string and convert it to lower case
+                    String secondStr = secondCell.getText();
+                    String thirdStr = thirdCell.getText();
+                    secondStr = secondStr.toLowerCase();
+                    thirdStr = thirdStr.toLowerCase();
+
+                    // Check cell values
+                    if (secondStr.equals("cst363")
+                            && thirdStr.equals("2")) {
+                        idx = i;
+                        found = true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Navigate to Assignments of row with desired values
+        WebElement row = rows.get(idx);
+        WebElement assignments = row.findElement(By.id("assignments"));
+        assignments.click();
+        Thread.sleep(SLEEP_DURATION);
+
+        // Select instructor dropdown menu
+        WebElement instDropdown = driver.findElement(By.xpath("//label[text()='Select Instructor']/following-sibling::div"));
+        instDropdown.click();
+        Thread.sleep(SLEEP_DURATION);
+
+        // Select instructor
+        WebElement inst = driver.findElement(By.xpath("//div[text()='jgross@csumb.edu']"));
+        inst.click();
+        Thread.sleep(SLEEP_DURATION);
+
+        // Select section dropdown menu
+        WebElement sectDropdown = driver.findElement(By.xpath("//label[text()='Select Section']/following-sibling::div"));
+        sectDropdown.click();
+        Thread.sleep(SLEEP_DURATION);
+
+        // Select section
+        WebElement sect = driver.findElement(By.xpath("//div[text()='9']"));
+        sect.click();
+        Thread.sleep(SLEEP_DURATION);
+
+        // Press add assignment
+        WebElement addButton = driver.findElement(By.xpath("//button[text()='Add Assignment']"));
+        addButton.click();
+        Thread.sleep(SLEEP_DURATION);
+
+        // Add title and due date
+        driver.findElement(By.id("title")).sendKeys("Extra Credit Assignment 1");
+        driver.findElement(By.id("dueDate")).clear();
+        driver.findElement(By.id("dueDate")).sendKeys("2024-07-04");
+
+        // Click save
+        driver.findElement(By.xpath("//button[text()='Save']")).click();
+        Thread.sleep(SLEEP_DURATION);
+
+        // Check if the page notifies you that an assignment was added successfully
+        String h4AddText = driver.findElement(By.tagName("h4")).getText();
+        h4AddText = h4AddText.toLowerCase();
+        assertTrue(h4AddText.contains("add"),
+                "Page doesn't update notification message after adding an assignment");
+
+        // Verify that the assignment has been added
+        driver.findElement(By.id("home")).click();
+        we = driver.findElement(By.id("sections"));
+        we.click();
+        Thread.sleep(SLEEP_DURATION);
+        driver.findElement(By.id("year")).sendKeys("2024");
+        driver.findElement(By.id("semester")).sendKeys("Spring");
+        Thread.sleep(SLEEP_DURATION);
+        assignments.click();
+        Thread.sleep(SLEEP_DURATION);
+        instDropdown.click();
+        Thread.sleep(SLEEP_DURATION);
+        inst.click();
+        Thread.sleep(SLEEP_DURATION);
+        sectDropdown.click();
+        Thread.sleep(SLEEP_DURATION);
+        sect.click();
+        Thread.sleep(SLEEP_DURATION);
+        // Grab all rows from assignment table
+        List<WebElement> assignRows = driver.findElements(By.xpath("//table/tbody/tr"));
+        // Search the table for Extra Credit Assignment 1 and 2024-07-04
+        // Set boolean in case we find what we're searching for
+        // Set boolean for confirming cleanup
+        boolean exFound = false;
+        boolean isDeleted = false;
+        for (int i = 0; !exFound && i < assignRows.size(); i++){
+            WebElement assignRow = assignRows.get(i);
+            // Grab 2nd and 3rd elements in the row
+            String title = assignRow.findElement(By.xpath("./td[2]")).getText();
+            String date = assignRow.findElement(By.xpath("./td[3]")).getText();
+
+            // Confirm if they match with what we're looking for
+            if (title.equals("Extra Credit Assignment 1") && date.equals("2024-07-04")){
+                exFound = true;
+
+                // After the boolean was confirmed true, delete the assignment made during
+                // delete the assignment made during the test
+                WebElement deleteButton = assignRow.findElement(By.xpath(".//button[contains(text(), 'Delete')]"));
+                deleteButton.click();
+                isDeleted = true;
+
+                // Check if the page notifies you that an assignment was deleted successfully
+                String h4DelText = driver.findElement(By.tagName("h4")).getText();
+                h4DelText = h4DelText.toLowerCase();
+                assertTrue(h4DelText.contains("delete"),
+                        "Page doesn't update notification message after deleting an assignment");
+            }
+        }
+        // If found, assert true otherwise state it wasn't found
+        assertTrue(exFound, "Assignment not found.");
+
+        // Clean up portion
+        assertTrue(isDeleted, "Clean up test failed, Assignment was not deleted.");
+        driver.findElement(By.id("home")).click();
+    }
+    // --- INSTRUCTOR TESTS END ---
 }
